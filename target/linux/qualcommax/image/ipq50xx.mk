8390bc5f5426121d8b6fb9b1d75d55080878c59c
Scott Mercer <TheRootEd24@gmail.com>
2024-10-04 12:13:05 -0400

    add initial glinet-b3000 support
    Signed-off-by: Scott Mercer <TheRootEd24@gmail.com>

--- target/linux/qualcommax/image/ipq50xx.mk
+++ target/linux/qualcommax/image/ipq50xx.mk
@@ -1,3 +1,20 @@
+define Device/glinet_gl-b3000
+  $(call Device/FitImage)
+  $(call Device/UbiFit)
+  SOC := ipq5018
+  DEVICE_VENDOR := GL.iNET
+  DEVICE_MODEL := GL-B3000
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  DEVICE_DTS_CONFIG := config@mp03.5-c1
+  UBINIZE_OPTS := -E 5
+  IMAGES := nand-factory.ubi
+  DEVICE_PACKAGES := \
+  	ath11k-firmware-qcn6122 \
+  	ipq-wifi-glinet_gl-b3000 \
+endef
+TARGET_DEVICES += glinet_gl-b3000
+
 define Device/linksys_mx2000
 	$(call Device/FitImageLzma)
 	DEVICE_VENDOR := Linksys
